36d46eb4f73b4169863d4e0626114bcee004eddc
Tezkerek <andrei.ancuta@gmail.com>
2018-12-28 19:21:42 +0200

    Permission that verifies if request user matches the user object

--- users/permissions.py
+++ users/permissions.py
@@ -1,8 +1,19 @@
 from rest_framework.permissions import BasePermission
 
+class IsUser(BasePermission):
+    """
+    For use with a user queryset.
+    The object must be the request user.
+    """
+    message = "You can only do this to yourself."
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj
+
 class IsUserOrGroupAdmin(BasePermission):
     """
     For use with a user queryset.
+    The request user must be a group admin or the object itself.
     """
     message = "You must be the user or the group admin to perform this operation."
 
